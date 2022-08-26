cb14d5e9c3c4cd5f8102deaf3aba85462f507fc9
Dirk Kok <the@dirkkok.nl>
2022-08-26 12:32:18 +0200

    de entitize text nodes

--- ObservancesBot/HtmlUtil.cs
+++ ObservancesBot/HtmlUtil.cs
@@ -4,17 +4,17 @@ using HtmlAgilityPack;
 namespace ObservancesBot;
 
 public static class HtmlUtil {
-	public static IText GetText(this HtmlNode node, Uri baseHref, Func<HtmlNode, bool> filter) {
+	public static IText GetText(this HtmlNode node, Uri? baseHref = null, Func<HtmlNode, bool>? filter = null) {
 		if (node.NodeType == HtmlNodeType.Element) {
-			var childTexts = new CompositeText(node.ChildNodes.Where(filter).Select(childNode => GetText(childNode, baseHref, filter)).ToList());
+			var childTexts = new CompositeText(node.ChildNodes.Where(filter ?? (_ => true)).Select(childNode => GetText(childNode, baseHref, filter)).ToList());
 
 			return node.Name switch {
-				"a" => new LinkText(new Uri(baseHref, node.GetAttributeValue("href", null)), childTexts),
+				"a" => new LinkText(baseHref == null ? new Uri(node.GetAttributeValue("href", null)) : new Uri(baseHref, node.GetAttributeValue("href", null)), childTexts),
 				"i" => new StyledText(Style.Italic, childTexts),
 				_ => childTexts
 			};
 		} else { // node.NodeType == HtmlNodeType.Text
-			return new LiteralText(node.InnerText);
+			return new LiteralText(HtmlEntity.DeEntitize(node.InnerText));
 		}
 	}
 }
