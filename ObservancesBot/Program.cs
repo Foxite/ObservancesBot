4156e259d864c2729ca5cfee2c50c6dc35a62b45
Dirk Kok <the@dirkkok.nl>
2023-02-03 02:06:27 +0100

    save to csv

--- ObservancesBot/Program.cs
+++ ObservancesBot/Program.cs
@@ -13,18 +13,42 @@ var http = new HttpClient() {
 	}
 };
 
-Target target = new DiscordWebhookTarget();
+Target target = new SaveToCsvTarget("/home/foxite/observances.csv");
 ObservanceService observanceService = source switch {
 	"wikipedia" => new WikipediaObservanceService(http),
 	"daysoftheyear.com" => new DaysOfTheYearDotComObservanceService(http)
 };
 
+async Task SendAllObservances() {
+	for (var date = new DateTime(2020, 01, 01); date.Year == 2020; date = date.AddDays(1)) {
+		Console.WriteLine(date.ToString("yyyy-MM-dd"));
+		IReadOnlyCollection<IText>? observances = await observanceService.GetObservances(date);
 
-DateTime date = DateTime.Today;
-IReadOnlyCollection<IText>? observances = await observanceService.GetObservances(date);
+		if (observances == null) {
+			Console.WriteLine("null");
+		} else {
+			await target.Send(new Observances(observances, date, observanceService.GetSourceUri(date), observanceService.Name));
+		}
+	}
+}
 
-if (observances == null) {
-	Console.WriteLine("null");
-} else {
-	await target.Send(new Observances(observances, date, observanceService.GetSourceUri(date), observanceService.Name));
+async Task SendTodaysObservances() {
+	DateTime date = DateTime.Today;
+	IReadOnlyCollection<IText>? observances = await observanceService.GetObservances(date);
+	if (observances == null) {
+		Console.WriteLine("null");
+	} else {
+		await target.Send(new Observances(observances, date, observanceService.GetSourceUri(date), observanceService.Name));
+	}
+}
+
+await SendTodaysObservances();
+
+// ReSharper disable once SuspiciousTypeConversion.Global
+if (observanceService is IDisposable disposableService) {
+	disposableService.Dispose();
+}
+
+if (target is IDisposable disposableTarget) {
+	disposableTarget.Dispose();
 }
---
+++ ObservancesBot/Services/SaveToCsvTarget.cs
@@ -0,0 +1,41 @@
+using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+using Foxite.Text;
+
+namespace ObservancesBot;
+
+public class SaveToCsvTarget : Target, IDisposable {
+	private readonly CsvWriter m_CsvWriter;
+
+	public SaveToCsvTarget(string path) {
+		var sw = new StreamWriter(path);
+		m_CsvWriter = new CsvWriter(sw, new CsvConfiguration(CultureInfo.InvariantCulture) {
+			Delimiter = ","
+		}, false);
+
+		m_CsvWriter.WriteHeader<CsvRow>();
+	}
+
+	public async override Task Send(Observances observances) {
+		var formatter = new DiscordTextFormatter();
+		foreach (IText item in observances.Items) {
+			await m_CsvWriter.NextRecordAsync();
+			m_CsvWriter.WriteRecord(new CsvRow(observances.Date, formatter.Format(item)));
+		}
+	}
+
+	public void Dispose() {
+		m_CsvWriter.Dispose();
+	}
+	
+	private class CsvRow {
+		public DateTime Date { get; }
+		public string Text { get; }
+	
+		public CsvRow(DateTime date, string text) {
+			Date = date;
+			Text = text;
+		}
+	}
+}
