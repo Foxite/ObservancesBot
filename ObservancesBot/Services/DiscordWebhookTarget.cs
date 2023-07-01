c5c74b4e7879b85e083df8b8dd13c6c453d51d69
Dirk Kok <the@dirkkok.nl>
2023-07-01 17:49:41 +0200

    add alternative format for discord, using markdown list

--- ObservancesBot/Services/DiscordWebhookTarget.cs
+++ ObservancesBot/Services/DiscordWebhookTarget.cs
@@ -7,9 +7,11 @@ namespace ObservancesBot;
 public class DiscordWebhookTarget : Target {
 	private readonly ITextFormatter m_Formatter;
 	private readonly List<DiscordWebhookClient> m_WebhookClients;
+	private readonly bool m_UseFields;
 
 	public DiscordWebhookTarget() {
 		string webhookUrl = Util.GetEnv("WEBHOOK_URL");
+		m_UseFields = Util.GetEnv("DISCORD_USE_FIELDS") == "true";
 		
 		m_Formatter = ModularTextFormatter.Markdown();
 
@@ -18,29 +20,63 @@ public class DiscordWebhookTarget : Target {
 
 	public async override Task Send(Observances observances) {
 		var fields = new List<EmbedFieldBuilder>();
-		
-		int i = 1;
-		foreach (IText observance in observances.Items) {
-			if (i >= 25) {
-				fields.Add(new EmbedFieldBuilder() {
-					Name = "And more",
-					Value = m_Formatter.Format(new LinkText(observances.SourceUri, new LiteralText("Check the source!")))
-				});
-				break;
-			} else {
-				fields.Add(new EmbedFieldBuilder() {
-					Name = i.ToString(),
-					Value = m_Formatter.Format(observance)
-				});
+		string description;
+
+		if (m_UseFields) {
+			description = "Good morning! Around the world, these celebrations will happen today:";
+			
+			int i = 1;
+			foreach (IText observance in observances.Items) {
+				if (i >= 25) {
+					fields.Add(new EmbedFieldBuilder() {
+						Name = "And more",
+						Value = m_Formatter.Format(new LinkText(observances.SourceUri, new LiteralText("Check the source!")))
+					});
+				} else {
+					fields.Add(new EmbedFieldBuilder() {
+						Name = i.ToString(),
+						Value = m_Formatter.Format(observance)
+					});
+				}
+
+				i++;
+			}
+		} else {
+			string RenderDescription(IReadOnlyList<IText> items) {
+				return m_Formatter.Format(new CompositeText(new LiteralText("Good morning! Around the world, these celebrations will happen today:\n"), new ListText(true, items)));
 			}
 
-			i++;
+			List<IText> items = new List<IText>();
+
+			bool ExceedsLimit() {
+				const int maxEmbedDescriptionLength = 4096;
+				return RenderDescription(items).Length > maxEmbedDescriptionLength;
+			}
+
+			items.AddRange(observances.Items);
+
+			IText andMoreText = new CompositeText(new LiteralText("And more - "), new LinkText(observances.SourceUri, new LiteralText("check the source!")));
+
+			bool notFirstIteration = false;
+			while (ExceedsLimit()) {
+				if (notFirstIteration) {
+					// Remove "and more" text
+					items.RemoveAt(items.Count - 1);
+				}
+
+				items.RemoveAt(items.Count - 1);
+				items.Add(andMoreText);
+
+				notFirstIteration = true;
+			}
+
+			description = RenderDescription(items);
 		}
 
 		Embed[] embeds = new[] {
 			new EmbedBuilder()
 				.WithTitle($"{observances.Date:MMMM} {observances.Date:dd}")
-				.WithDescription("Good morning! Around the world, these celebrations will happen today:")
+				.WithDescription(description)
 				.WithFooter($"Source: {observances.SourceName}")
 				.WithUrl(observances.SourceUri.ToString())
 				.WithFields(fields)
